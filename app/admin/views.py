063bdf00c6c158d5699cbd46c95f372c2f90cb56
Vladyslav Velychko <velyk95@gmail.com>
2017-03-20 01:45:31 +0200

    adding error pages

--- app/admin/views.py
+++ app/admin/views.py
@@ -140,3 +140,21 @@ def list_employees():
 	employees=Employee.query.all()
 	return render_template('/admin/employees/employees.html',employees=employees,title='Employees')
 @admin.route('/employees/assign/<int:id>',methods=['GET','POST'])
+@login_required
+def assign_employee(id):
+	check_admin()
+	employee=Employee.query.get_or_404(id)
+	if employee.is_admin:
+		abort(403)
+
+	form=EmployeeAssignForm(obj=employee)
+	if form.validate_on_submit():
+		employee.department=form.department.data
+		employee.role=form.role.data
+		db.session.add(employee)
+		db.session.commit()
+		flash('You have successfully assigned a department and role.')
+
+		return redirect(url_for('admin.list_employees'))
+
+	return render_template('admin/employees/employee.html',employee=employee,form=form,title='Assign Employee')
